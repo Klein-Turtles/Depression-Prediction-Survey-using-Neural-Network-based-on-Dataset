ef248b0b7ff904574a3223deb25c7cd179e4ae1d
Klein-Turtles <tegarjr634@gmail.com>
2024-12-08 10:25:41 +0700

    Update app.py

--- LastPlease/app.py
+++ LastPlease/app.py
@@ -43,7 +43,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('survey.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
